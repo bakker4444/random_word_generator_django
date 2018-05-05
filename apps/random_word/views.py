03114a267e0ea5de67bdf1f33a145bb067be8ec3
bakker4444 <bakker4444@gmail.com>
2018-05-04 20:24:49 -0700

    indentation error at views.py

--- apps/random_word/views.py
+++ apps/random_word/views.py
@@ -7,15 +7,17 @@ from django.utils.crypto import get_random_string
 # Create your views here.
 def index(request):
     if "random_count" not in request.session.keys():
+        print "first_visit"
         request.session["random_count"] = 0
     else:
         print "here 1"
         request.session["random_count"] += 1
-        context = {
-            "random_count" : request.session["random_count"],
-            "random_number" : get_random_string(length=14),
-        }
-        return render(request, "random_word/index.html", context)
+    context = {
+        "random_count" : request.session["random_count"],
+        "random_number" : get_random_string(length=14),
+    }
+    return render(request, "random_word/index.html", context)
+
 
 def reset(request):
     print "here 2"
